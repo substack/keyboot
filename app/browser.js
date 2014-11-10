e10a1db2ca7486e9bc7ba048b72733a100eba029
James Halliday <mail@substack.net>
2014-11-09 17:06:03 -0800

    spinner, click button to generate keys

--- app/browser.js
+++ app/browser.js
@@ -1,13 +1,31 @@
 var gen = require('./lib/gen.js');
+var Spinner = require('spinner-browserify');
 
-(function () {
+var button = document.querySelector('#splash button');
+button.addEventListener('click', function (ev) {
+    generate();
+});
+
+function generate () {
+    button.style.display = 'none';
+    
     var opts = {
         numBits: 4096,
         userId: 'User Name <username@example.com>',
         passphrase: 'whatever...'
     };
+    
+    var spin = new Spinner();
+    
+    var busy = document.querySelector('#splash .busy');
+    var msg = document.querySelector('#splash .msg');
+    msg.textContent = 'Generating keypair. Please wait.';
+    busy.appendChild(spin.el);
+    
     gen(opts, function (err, keypair) {
-        console.log(keypair);
-        window.keypair = keypair;
+        spin.stop();
+        busy.removeChild(spin.el);
+        msg.textContent = 'keypair generated';
+        console.log('ok...');
     });
-})();
+}
